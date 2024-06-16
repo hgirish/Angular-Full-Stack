07f497cadc9475c57897b409344a0ae98c77cfa0
Girish <girish@post.com>
2024-06-15 21:28:41 -0700

    express

---
+++ server/app.ts
@@ -0,0 +1,22 @@
+import  { Express, Request, Response } from "express";
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port =  3000;
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+
+//export {app}
