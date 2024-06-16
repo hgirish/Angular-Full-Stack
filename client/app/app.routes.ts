113d079a23b5eae3ba1628a0d5e4f75457eb4151
Girish <girish@post.com>
2024-06-15 20:36:17 -0700

    test passes

--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CatsComponent } from './cats/cats.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: AboutComponent,
+        title: 'About Angular Full Stack'
+    },
+    {
+        path: 'cats',
+        component: CatsComponent,
+        title: 'Cats'
+    }
+];
